2c9024888e842fb0f7148eaa3099d24eb2cccd15
deaving-71 <110570275+deaving-71@users.noreply.github.com>
2024-03-21 22:16:55 +0100

    delete test file

--- server/src/test.js
+++
@@ -1,7 +0,0 @@
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-for (let index = 0; index < 20; index++) {
-  console.log(randomIntFromInterval(1, 2));
-}
